3a4049dc0dbf27bb8540522968bca5365e0f0b1c
Peter Halliday <pghalliday@gmail.com>
2016-04-17 19:38:48 +0200

    using C++ 2011 features

--- cpp/variables/main.cpp
+++ cpp/variables/main.cpp
@@ -2,14 +2,13 @@
 using namespace std;
 
 int main () {
-  int a, b;
+  int a=5;
+  int b(3);
+  int c{2};
   int result;
 
-  a = 5;
-  b = 2;
-  a = a + 1;
-  result = a - b;
-
+  a = a + b;
+  result = a - c;
   cout << result;
 
   return 0;
